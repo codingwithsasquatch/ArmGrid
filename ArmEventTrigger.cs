0fc96385230591328eb3aa3720c9d0384f723553
Sasha Rosenbaum <sasrose@microsoft.com>
2018-04-11 14:33:55 -0700

    fix the rest call to arm

--- ArmEventTrigger.cs
+++ ArmEventTrigger.cs
@@ -15,97 +15,106 @@ using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.Rest;
+using System.Configuration;
 
 namespace ArmGrid
 {
-    public static class ArmEventTrigger
-    {
+	public static class ArmEventTrigger
+	{
 
-        private static readonly HttpClient HttpClient = new HttpClient();
-        //TODO: ad app settings reference
-        private static readonly string EventGridEndpoint = "app setting variable";
+		private static readonly HttpClient HttpClient = new HttpClient();
+		//TODO: ad app settings reference
+		private static readonly string EventGridEndpoint = ConfigurationManager.AppSettings["EventGridCustomTopicEndpoint"];
 
-        [FunctionName("ArmEventTrigger")]
-        public static async Task <IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
-        {
-            string requestContent = await req.Content.ReadAsStringAsync();
+		[FunctionName("ArmEventTrigger")]
+		public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
+		{
+			string requestContent = await req.Content.ReadAsStringAsync();
 
-            //TODO: lookup response code for event grid to get EG to resubmit it since it failed delivery
-            return await ReRaiseEventGridEvent(requestContent, log)
-                ? (ActionResult)new OkObjectResult("")
-                : new BadRequestObjectResult("It's busted");
-        }
+			//TODO: lookup response code for event grid to get EG to resubmit it since it failed delivery
+			return await ReRaiseEventGridEvent(requestContent, log)
+				? (ActionResult)new OkObjectResult("")
+				: new BadRequestObjectResult("It's busted");
+		}
 
-        private static async Task<bool> ReRaiseEventGridEvent(string originalEvent, TraceWriter log)
-        {
-            EventGridEvent[] eventGridEvents = JsonConvert.DeserializeObject<EventGridEvent[]>(originalEvent);
+		private static async Task<bool> ReRaiseEventGridEvent(string originalEvent, TraceWriter log)
+		{
+			EventGridEvent[] eventGridEvents = JsonConvert.DeserializeObject<EventGridEvent[]>(originalEvent);
 
-            foreach (EventGridEvent eventGridEvent in eventGridEvents)
-            {
-                if (eventGridEvent.EventType == "Microsoft.Resources.ResourceWriteSuccess") {
-                    JObject dataObject = eventGridEvent.Data as JObject;
-                    string subscriptionId = (string) dataObject["subscriptionId"];
-                    string resourceUri = (string) dataObject["resourceUri"];
-                    log.Info(subscriptionId);
-                    var armData = await GetArmResource();
-                    //append armData to the end of the Data object
-                }
+			foreach (EventGridEvent eventGridEvent in eventGridEvents)
+			{
 
-                var newEvent = new EventGridEvent()
-                {
-                    Topic = eventGridEvent.Topic,
-                    Subject = eventGridEvent.Subject,
-                    EventType = eventGridEvent.EventType,
-                    EventTime = eventGridEvent.EventTime,
-                    Id = eventGridEvent.Id,
-                    DataVersion = eventGridEvent.DataVersion,
-                    Data = eventGridEvent.Data
-                };
+				JObject dataObject = eventGridEvent.Data as JObject;
 
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, EventGridEndpoint)
-                {
-                    Content = new StringContent(JsonConvert.SerializeObject(newEvent), Encoding.UTF8, "application/json")
-                };
-
-                HttpResponseMessage response = await HttpClient.SendAsync(request);
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception($"Exception HTTP Response {response.StatusCode}");
-                }
-            }
-            //TODO: return true if we are good return false if we want event grid the the resubmit
-            return true;
-        }
-
-        private static async Task <string> GetArmResource()
-        {
-            //TODO: fix references and make this shizzle work
-            var eventData = dataObject.ToObject<ResourceWriteSuccessData>();
-				log.Info($"Got VM event data {eventData}");
-
-				var azureServiceTokenProvider = new AzureServiceTokenProvider();
-
-				try
+				if (eventGridEvent.EventType == "Microsoft.Resources.ResourceWriteSuccess")
 				{
-					var serviceCreds = new TokenCredentials(await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com/").ConfigureAwait(false));
-
-					var resourceManagementClient =
-						new ResourceManagementClient(serviceCreds) { SubscriptionId = subscriptionId };
-
-					var resourceState = await resourceManagementClient.Resources.GetByIdAsync(eventData.ResourceUri, "2017-12-01");
-					var properties = resourceState.Properties.ToString()
-						.Replace(Environment.NewLine, String.Empty)
-						.Replace("\\", String.Empty).Replace(" ", String.Empty);
-
-					log.Info(properties);
-					response = properties;
-
+					
+					string subscriptionId = (string)dataObject["subscriptionId"];
+					string resourceUri = (string)dataObject["resourceUri"];
+					log.Info(subscriptionId);
+					var resourceState = await GetArmResourceState(subscriptionId, resourceUri, log);
+					
+					//append armData to the end of the Data object
+					dataObject.Add("ResourceState", resourceState);
 				}
-				catch (Exception exp)
+
+				var newEvent = new EventGridEvent()
 				{
-					log.Info($"Something went wrong: {exp.Message}");
+					Topic = eventGridEvent.Topic,
+					Subject = eventGridEvent.Subject,
+					EventType = eventGridEvent.EventType,
+					EventTime = eventGridEvent.EventTime,
+					Id = eventGridEvent.Id,
+					DataVersion = eventGridEvent.DataVersion,
+					Data = dataObject
+				};
+
+				HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, EventGridEndpoint)
+				{
+					Content = new StringContent(JsonConvert.SerializeObject(newEvent), Encoding.UTF8, "application/json")
+				};
+
+				HttpResponseMessage response = await HttpClient.SendAsync(request);
+				if (!response.IsSuccessStatusCode)
+				{
+					throw new Exception($"Exception HTTP Response {response.StatusCode}");
 				}
-            return "";
-        }
-    }
+			}
+			//TODO: return true if we are good return false if we want event grid the the resubmit
+			return true;
+		}
+
+
+		private static async Task<string> GetArmResourceState(string subscriptionId, string resourceId, TraceWriter log)
+		{
+
+			var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+			try
+			{
+				var serviceCreds = new TokenCredentials(await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com/").ConfigureAwait(false));
+
+				var resourceManagementClient =
+					new ResourceManagementClient(serviceCreds) { SubscriptionId = subscriptionId };
+
+				var resourceState = await resourceManagementClient.Resources.GetByIdAsync(resourceId, "2017-12-01");
+				var properties = resourceState.Properties.ToString()
+					.Replace(Environment.NewLine, String.Empty)
+					.Replace("\\", String.Empty).Replace(" ", String.Empty);
+
+				log.Info(properties);
+				return properties;
+
+			}
+			catch (Exception exp)
+			{
+				var message = $"Something went wrong: {exp.Message}";
+				log.Info(message);
+				return message;
+			}
+		
+			
+		}
+	}
 }
